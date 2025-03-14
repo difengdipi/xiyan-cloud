a006887bc8968345486e844e5951acf81c80aebb
zh <2358853434@qq.com>
2025-03-15 01:28:42 +0800

    商城模块商品管理实现

---
+++ ruoyi-modules/ruoyi-category/src/main/java/com/ruoyi/category/domain/Goods.java
@@ -0,0 +1,22 @@
+package com.ruoyi.category.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName(autoResultMap = true)
+public class Goods {
+    @TableId(type = IdType.AUTO)
+    private Integer goodsId;
+    private String goodsName;
+    private String goodsDesc;
+    private Double goodsPrice;
+    private String goodsPicture;
+    private Integer goodsOrdernum;
+}
