0e1556213629cbf552639360166b57b1ece70a79
zh <2358853434@qq.com>
2025-03-27 15:43:57 +0800

    商城服务订单功能

---
+++ ruoyi-modules/ruoyi-shop/src/main/java/com/ruoyi/shop/controller/order/OrderController.java
@@ -0,0 +1,16 @@
+package com.ruoyi.shop.controller.order;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @author: zh
+ * @Create : 2025/3/26
+ * @Project_name : RuoYi-Cloud
+ * @Version :
+ **/
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+}
