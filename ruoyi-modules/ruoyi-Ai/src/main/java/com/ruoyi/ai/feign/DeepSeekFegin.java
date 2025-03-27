8512132420b9474ebb3614a82f09024ea1908664
zh <2358853434@qq.com>
2025-03-27 15:53:29 +0800

    Ai微服务调用deepseek的远程接口

--- ruoyi-modules/ruoyi-Ai/src/main/java/com/ruoyi/ai/feign/DeepSeekFegin.java
+++ ruoyi-modules/ruoyi-Ai/src/main/java/com/ruoyi/ai/feign/DeepSeekFegin.java
@@ -1,6 +1,8 @@
 package com.ruoyi.ai.feign;
 
+import com.ruoyi.ai.feign.fallback.DeepSeekFeginFallback;
 import com.ruoyi.ai.pojos.ChatRequest;
+import com.ruoyi.common.core.domain.R;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,9 +17,9 @@ import org.springframework.web.bind.annotation.RequestHeader;
  * @Version :
  **/
 @Component
-@FeignClient(name = "deepseek",url="https://api.deepseek.com/v1")
+@FeignClient(name = "deepseek",url="https://api.deepseek.com/v1" , fallbackFactory = DeepSeekFeginFallback.class)
 public interface DeepSeekFegin {
 
     @PostMapping("/chat/completions")
-    public String getContent(@RequestHeader("Authorization") String authorization, @RequestBody ChatRequest chatRequest);
+    public R getContent(@RequestHeader("Authorization") String authorization, @RequestBody ChatRequest chatRequest);
 }
