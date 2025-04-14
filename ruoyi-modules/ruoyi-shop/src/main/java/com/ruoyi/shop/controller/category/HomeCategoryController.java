35c86b998d404bec55463d42494f090fe9c284be
zh <2358853434@qq.com>
2025-04-15 00:36:50 +0800

    fix：修改默认随机时间

--- ruoyi-modules/ruoyi-shop/src/main/java/com/ruoyi/shop/controller/category/HomeCategoryController.java
+++ ruoyi-modules/ruoyi-shop/src/main/java/com/ruoyi/shop/controller/category/HomeCategoryController.java
@@ -1,18 +1,23 @@
 package com.ruoyi.shop.controller.category;
 
 import com.ruoyi.common.core.domain.R;
+import com.ruoyi.common.redis.service.RedisService;
 import com.ruoyi.shop.domain.category.Category;
 import com.ruoyi.shop.domain.category.vo.CategoryItemVo;
 import com.ruoyi.shop.service.category.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description: 小程序home分类controller
@@ -24,13 +29,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/home/category")
 @Tag(name = "小程序homeController")
+@Slf4j
 public class HomeCategoryController {
 
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    RedisService redisService;
+    private static final String CATEGORY_KEY = "Home_category";
+    @PostConstruct
+    public void init(){
+        log.info("HomeCategoryController Bean加载完成，开始初始化首页数据");
+        mutiliCategory();
+    }
     @GetMapping("/mutli")
     @Operation(summary = "前台分类")
     public R mutiliCategory(){
+        if(redisService.hasKey(CATEGORY_KEY)){
+            return R.ok( redisService.getCacheObject(CATEGORY_KEY));
+        }
         List<Category> list = categoryService.list();
         ArrayList<CategoryItemVo> categoryItemVos = new ArrayList<>();
         for (Category category : list) {
@@ -39,6 +56,7 @@ public class HomeCategoryController {
                     .name(category.getCategoryName()).build();
             categoryItemVos.add(build);
         }
+        redisService.setCacheObject(CATEGORY_KEY,categoryItemVos,ThreadLocalRandom.current().nextLong(20,50), TimeUnit.MINUTES);
         return R.ok(categoryItemVos);
     }
 }
