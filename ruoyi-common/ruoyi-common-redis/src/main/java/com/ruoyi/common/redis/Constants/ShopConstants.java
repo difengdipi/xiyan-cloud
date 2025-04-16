decf826f06b442a20ec2e51445dab2a555558643
zh <2358853434@qq.com>
2025-04-16 19:24:55 +0800

    添加redisson公共依赖

---
+++ ruoyi-common/ruoyi-common-redis/src/main/java/com/ruoyi/common/redis/Constants/ShopConstants.java
@@ -0,0 +1,18 @@
+package com.ruoyi.common.redis.Constants;
+
+/**
+ * @Description:
+ * @author: zh
+ * @Create : 2025/4/16
+ * @Project_name : RuoYi-Cloud
+ * @Version :
+ **/
+public class ShopConstants {
+    public static final String CACHE_KEY_PREFIX = "shop:";
+
+    public static final String CACHE_KEY_CATEGORY = "category:";
+
+
+    public static final String CACHE_KEY_ORDERINFO_LOCK = "ORDERINFO_LOCK:";
+
+}
---
+++ ruoyi-common/ruoyi-common-redis/src/main/java/com/ruoyi/common/redis/configure/RedissionService.java
@@ -0,0 +1,43 @@
+package com.ruoyi.common.redis.configure;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+/**
+ * @Description:
+ * @author: zh
+ * @Create : 2025/3/28
+ * @Project_name : RuoYi-Cloud
+ * @Version :
+ **/
+@Component
+@ConfigurationProperties
+@AutoConfigureBefore(RedissonConnectionFactory.class)
+public class RedissionService {
+    @Value("${spring.redis.host}")
+    public String host;
+    @Value("${spring.redis.port}")
+    public Integer port;
+    @Value("${spring.redis.password}")
+    public String password;
+
+    @Bean
+    public RedissonClient getRedissonClient() {
+        Config config = new Config();
+        if(StringUtils.isEmpty(password)){
+            config.useSingleServer().setAddress("redis://"+host+":"+port);
+        }else{
+            config.useSingleServer().setAddress("redis://"+host+":"+port).setPassword(password);
+        }
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+}
